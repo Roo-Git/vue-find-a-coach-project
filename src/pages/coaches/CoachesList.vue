8b63d2d7f19847902d392b2ec3ccb0231acfd552
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-09 13:28:13 +0200

    add vuex, coach module and coach data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,15 +5,24 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "Coaches-List",
+  computed: {
+    ...mapGetters({ filteredCoaches: ["coaches/getCoaches"] }),
+    ...mapGetters({ hasCoaches: ["coaches/hasCoaches"] }),
+  },
 };
 </script>
 
