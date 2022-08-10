e4bbfa85ed41fad57be2e711986f3bbaa3b8b250
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-10 08:10:11 +0200

    fix bug

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -5,8 +5,8 @@ export default {
   hasCoaches(state) {
     return state.coaches && state.coaches.length > 0;
   },
-  isCoach(getters, rootGetters) {
-    const coaches = getters.coaches;
+  isCoach(state, getters, rootGetters) {
+    const coaches = state.coaches || getters.coaches;
     const userId = rootGetters.userId;
     return coaches.some((coach) => coach.id === userId);
   },
