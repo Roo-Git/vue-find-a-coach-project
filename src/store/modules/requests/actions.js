8a662f84b913a4e45d827235fd36465f55b42540
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-10 16:03:19 +0200

    add post https method

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,13 +1,24 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: "r1",
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const url = `https://vue-find-a-coach-cea8e-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`;
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newRequest),
+    });
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to sent request");
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit("addRequest", newRequest);
   },
 };
-
-// https://vue-find-a-coach-cea8e-default-rtdb.firebaseio.com/
