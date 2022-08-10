6066fa156ae2603c7d40c28772df5bc70ddbf10b
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-10 08:06:17 +0200

    bug isCoach getter

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,7 +6,9 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button link to="/register">Register as Coach</base-button>
+        <base-button v-if="!isCoach" link to="/register"
+          >Register as Coach</base-button
+        >
       </div>
       <ul v-if="hasCoaches">
         <coach-item
@@ -46,6 +48,7 @@ export default {
   },
   computed: {
     ...mapGetters({ hasCoaches: ["coaches/hasCoaches"] }),
+    ...mapGetters({ isCoach: ["coaches/isCoach"] }),
     filteredCoaches() {
       const coaches = this.$store.getters["coaches/getCoaches"];
       return coaches.filter((coach) => {
