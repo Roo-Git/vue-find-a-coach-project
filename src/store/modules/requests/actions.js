58936a24c367720542770790cbe1f73eeb9d298f
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-10 16:49:24 +0200

    send coach request

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -21,4 +21,29 @@ export default {
 
     context.commit("addRequest", newRequest);
   },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const url = `https://vue-find-a-coach-cea8e-default-rtdb.firebaseio.com/requests/${coachId}.json`;
+    const response = await fetch(url);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to fetch request"
+      );
+      throw error;
+    }
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(request);
+    }
+    context.commit("setRequests", requests);
+  },
 };
