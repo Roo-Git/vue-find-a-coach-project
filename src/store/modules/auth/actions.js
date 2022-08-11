f0a2f5ff2a4513e871fe856c50e1437f87d3811e
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-11 10:52:26 +0200

    add login method

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,31 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+    const API_KEY = "AIzaSyBkSDnHqNnHHrb0W1gBYp8fZ0wt1BDlsFA";
+    const response = await fetch(`${url}${API_KEY}`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || "Failed to authenticate."
+      );
+      throw error;
+    }
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const url =
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
